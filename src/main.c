9b4c2406d1bc65ae4c1c355daf02a5ac63cddbbc
MarcoPENE <189098755+MarcoPENE@users.noreply.github.com>
2025-06-18 12:37:15 +0200

    Ajout color_invert à tester car pb de terminal

--- src/main.c
+++ src/main.c
@@ -91,7 +91,10 @@ int main(int argc, char **argv) {
   }
   if (strncmp(configuration.command, "color_blue", 10) == 0) {
     color_blue(configuration.filenames[0]);
-  } 
+  }
+  if (strncmp(configuration.command, "color_invert", 12)==0){
+    color_invert(configuration.filenames[0]);
+  }
    /* TO COMPLETE
    */
   
